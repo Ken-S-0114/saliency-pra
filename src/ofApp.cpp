fabc49c66675c33655eca1092a090d6799e20392
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-07-05 19:31:50 +0900

    first commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,78 @@
+#include "ofApp.h"
+#include "ofxCv.h"
+#include <opencv2/saliency/saliencyBaseClasses.hpp>
+#include <opencv2/saliency/saliencySpecializedClasses.hpp>
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+//  image = cv::imread("test.png");
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+//  cv::Ptr<cv::saliency::Saliency> saliencyAlgorithm =
+//      cv::saliency::Saliency::create( saliency_algorithm );
+//  saliencyAlgorithm->computeSaliency( image, saliencyMap );
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
